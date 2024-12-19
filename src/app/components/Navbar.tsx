557bb1423b829691694bb942bba738893b3ad064
HunterJoe <hunterjoe9999@gmail.com>
2024-12-19 16:54:42 +0900

    FEAT: Dark mode added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Logo from '@/public/logo.png';
-import { Button } from '@/components/ui/button';
+
 import { AuthModal } from './AuthModal';
+import { ThemeToggle } from './ThemeToggle';
+import { Input } from '@/components/ui/input';
 
 export default function Navbar() {
   return (
@@ -13,24 +15,13 @@ export default function Navbar() {
           <span className="text-primary">Dev'</span>rium
         </h4>
       </Link>
-      <div> search bar</div>
-      <AuthModal />
+      <div>
+        <Input placeholder="인풋태그임" />
+      </div>
+      <div>
+        <AuthModal />
+        <ThemeToggle />
+      </div>
     </div>
   );
-}
-
-{
-  /* <div className="mx-auto flex max-w-screen-2xl items-center justify-between py-2">
-      <Link href="/" className="flex items-center gap-2">
-        <Image src={Logo} alt="Deverium logo" className="size-12" />
-        <h4 className="text-3xl font-semibold">
-          <span className="text-primary">Devrium</span>
-        </h4>
-      </Link>
-      <div>
-        <Link href="/login">
-          <Button>Log In</Button>
-        </Link>
-      </div>
-    </div> */
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { ThemeProvider } from './components/ThemeProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,7 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
         <Footer />
       </body>
     </html>
