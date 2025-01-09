24fbe6d8f29bed0548b3efecdddeca5854708a63
HunterJoe <hunterjoe9999@gmail.com>
2025-01-09 17:11:30 +0900

    FEAT: 모바일용 사이드바 추가

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -6,19 +6,47 @@ import AuthModal from './AuthModal';
 import Avatar from './Avatar';
 import SearchBar from './SearchBar';
 import { ThemeToggle } from './ThemeToggle';
+import { Menu } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Button } from '../ui/button';
+import { LeftSidebarMenu } from '../layout/LeftSidebarMenu';
 
 export default async function Navbar() {
   const session = await validateUser();
 
   return (
     <div className="mx-auto flex h-14 max-w-full items-center justify-between border-b bg-muted/40 px-4 py-5 sm:px-6 lg:px-8">
-      <Link href="/" className="flex items-center gap-2">
-        <Image src={Logo} alt="logo" className="size-10" />
-        <h4 className="text-2xl font-semibold">
-          <span className="text-primary">Dev'</span>rium
-        </h4>
-      </Link>
-      <SearchBar />
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="shrink-0 md:hidden" size="icon" variant="outline">
+            <Menu className="size-5" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="flex flex-col">
+          <SheetTitle>Menu</SheetTitle>
+          <SheetDescription></SheetDescription>
+          <nav className="mt-10 grid gap-2">
+            <LeftSidebarMenu />
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="hidden md:block">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src={Logo} alt="logo" className="size-10" />
+          <h4 className="text-2xl font-semibold">
+            <span className="text-primary">Dev'</span>rium
+          </h4>
+        </Link>
+      </div>
+      <div className="hidden md:block">
+        <SearchBar />
+      </div>
       <div className="flex gap-2">
         {session ? (
           <Avatar image={session?.user.image} id={session.user.id} />
